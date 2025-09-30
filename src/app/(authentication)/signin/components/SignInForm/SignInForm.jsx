d4b9dd82251dacab362fa66a4f08c4aeff5841fa
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-30 23:25:35 +0600

    Signin form created

---
+++ src/app/(authentication)/signin/components/SignInForm/SignInForm.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+const SignInForm = () => {
+    // handleSubmit
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form className="w-full sm:w-[350px] md:w-[450px] lg:w-[550px] mx-auto" onSubmit={handleSubmit}>
+            {/* email */}
+            <div className="email mb-3">
+                <h1 className="text-sm text-slate-700 font-medium mb-2 ps-3">Email address:</h1>
+                <input type="email" name="email" id="email" autoComplete="off" placeholder="Email address" required className="w-full px-3 py-1.5 text-slate-800 border border-gray-300 focus:ring-3 ring-gray-200 rounded outline-none" />
+            </div>
+
+            {/* password */}
+            <div className="password mb-3">
+                <h1 className="text-sm text-slate-700 font-medium mb-2 ps-3">Password:</h1>
+                <input type="password" name="password" id="password" autoComplete="off" placeholder="Password" required className="w-full px-3 py-1.5 text-slate-800 border border-gray-300 focus:ring-3 ring-gray-200 rounded outline-none" />
+            </div>
+
+            <button className="w-full py-1.5 text-center font-medium text-white border border-blue-300 rounded bg-blue-500 hover:bg-blue-600 active:bg-blue-500">SIGN IN</button>
+        </form>
+    );
+};
+
+export default SignInForm;
---
+++ src/app/(authentication)/signin/page.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import SignInForm from "./components/SignInForm/SignInForm";
+
+const SignIn = () => {
+    return (
+        <div className="signIn">
+            <div className="container mx-auto w-full px-6 lg:px-8 py-8">
+                <h1 className="text-center text-2xl text-slate-700 font-medium">Sign in here</h1>
+
+                <div className="py-8">
+                    <SignInForm />
+                </div>
+
+                <div className="text-center text-sm text-gray-500">
+                    <h1>Don't Have An Account? <Link href={'/signup'} className="text-blue-500 hover:text-blue-600 active:text-blue-500 font-medium">SIGN UP</Link></h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
